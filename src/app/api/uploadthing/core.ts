d97d2de23f60fead3288056a23832f96843c54f8
D_Zucho <danny.nico.dev@outlook.com>
2024-06-01 02:06:22 -0700

    uploadthing file configuration

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -6,6 +6,8 @@ import { db } from '@/db'
 const f = createUploadthing()
 
 // FileRouter for your app, can contain multiple FileRoutes
+// FileRouter is a type that represents a collection of FileRoutes, or,
+//     in other words, a group of routes that handle file uploads.
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({ image: { maxFileSize: '4MB' } })
@@ -28,13 +30,40 @@ export const ourFileRouter = {
       const { width, height } = imgMetadata // get width and height
 
       if (!configId) {
-        const configureation = await db.configuration.create({
-          // data: {}
+        const configuration = await db.configuration.create({
+          data: {
+            imageUrl: file.url,
+            height: height || 500,
+            width: width || 500,
+            croppedImageUrl: file.url,
+          },
         })
-      }
+        return { configId: configuration.id }
+      } else {
+        // just update the existing configuration
+        const updatedConfiguration = await db.configuration.update({
+          where: {
+            id: configId,
+          },
+          data: {
+            croppedImageUrl: file.url,
+            // when image is cropped, update the croppedImageUrl to path
+          },
+        })
 
-      return { configId }
+        return { configId: updatedConfiguration.id }
+      }
     }),
 } satisfies FileRouter
 
 export type OurFileRouter = typeof ourFileRouter
+
+// Steps in onUploadComplete:
+// 1. Fetch the uploaded file
+// 2. Convert the file to a buffer. A buffer is a data structure that stores raw binary data.
+// 3. Get the image metadata. This includes information such as the width and height of the image.
+// 4. Get the width and height
+// 5. If no configId is provided, create a new configuration in the database.
+//       5b. configId is a unique identifier for the configuration
+// 6. If a configId is provided, update the existing configuration in the database
+// 7. Return the updated configuration id
