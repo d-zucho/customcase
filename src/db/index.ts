f9fb6af10ebf2ebc5834ce8775286d06474dd0c3
D_Zucho <danny.nico.dev@outlook.com>
2024-05-31 19:24:09 -0700

    connect uploadthing functionality - start neon console

---
+++ src/db/index.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client'
+
+declare global {
+  var cachedPrisma: PrismaClient
+}
+
+let prisma: PrismaClient
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient()
+  }
+
+  prisma = global.cachedPrisma
+}
+
+export const db = prisma
+
+// the purpose of this file is so that we don't have to create a new PrismaClient instance every time we want to interact with the database.
+
+// this file accomplishes the following:
+// 1. It imports the PrismaClient from the @prisma/client package.
+// 2. It declares a global variable cachedPrisma.
+// 3. It initializes a new PrismaClient instance and assigns it to the prisma variable.
+// 4. It checks if the NODE_ENV environment variable is set to production.
+// 5. If NODE_ENV is set to production, it creates a new PrismaClient instance.
+// 6. If NODE_ENV is not set to production, it checks if the global cachedPrisma variable is defined.
+// 7. If the global cachedPrisma variable is not defined, it creates a new PrismaClient instance and assigns it to the global cachedPrisma variable.
+// 8. It assigns the global cachedPrisma variable to the prisma variable.
+// 9. It exports the prisma variable as db.
+// 10. It ensures that only one PrismaClient instance is created in development mode.
